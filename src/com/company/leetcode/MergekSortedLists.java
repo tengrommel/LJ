d36a7017d2715e4df7a7139f83eb71ea75c7b540
zhouteng <wwwsousoucom@126.com>
2018-09-24 19:51:09 +0800

    Merge

---
+++ src/com/company/leetcode/MergekSortedLists.java
@@ -0,0 +1,29 @@
+package com.company.leetcode;
+import com.company.leetcode.ListNode;
+
+public class MergekSortedLists {
+
+	public ListNode mergeKLists(ListNode[] lists){
+		if (lists == null || lists.length == 0) return null;
+		return sort(lists, 0, lists.length-1);
+	}
+	public ListNode sort(ListNode[] lists, int lo, int hi){
+		if (lo>=hi) return lists[lo];
+		int mid = (hi - lo) / 2 + lo;
+		ListNode l1 = sort(lists, lo, mid);
+		ListNode l2 = sort(lists, mid+1, hi);
+		return merge(l1, l2);
+	}
+
+	public ListNode merge(ListNode l1, ListNode l2){
+		if (l1 == null) return l2;
+		if (l2 == null) return l1;
+		if (l1.val < l2.val){
+			l1.next = merge(l1.next, l2);
+			return l1;
+		}
+		l2.next = merge(l1, l2.next);
+		return l2;
+	}
+
+}
