ec1c105a7c310a2739da27a6e50bc99c657a04b2
zhouteng <wwwsousoucom@126.com>
2018-10-15 22:34:50 +0800

    file adapter

---
+++ src/com/teng/adapter/A2/FileIO.java
@@ -0,0 +1,9 @@
+package com.teng.adapter.A2;
+import java.io.IOException;
+
+public interface FileIO {
+	public void readFromFile(String filename) throws IOException;
+	public void writeToFile(String filename) throws IOException;
+	public void setValue(String key, String value);
+	public String getValue(String key);
+}
